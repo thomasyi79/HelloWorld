1484a2bb24e6eb45ffd72fafadb29526dea89202
Thomas Yi <thomasyi79@gmail.com>
2020-01-21 17:53:45 -0500

    added in comments and display messages.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display the "Hello World" Message
+            Console.WriteLine("Hello World");
+            // Display the second message of ISM 4300
+            Console.WriteLine("This is ISM 4300!");
+            Console.ReadKey(true);
+        }
+    }
+}
